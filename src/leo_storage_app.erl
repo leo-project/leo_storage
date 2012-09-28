2f5e37e11ab73d112d6a36b11090ef2ef23e4716
yosukehara <yousuke.hara@gmail.com>
2012-09-28 23:16:20 +0900

    Refactor regarding statistics

--- src/leo_storage_app.erl
+++ src/leo_storage_app.erl
@@ -52,6 +52,10 @@ stop(_State) ->
 %%----------------------------------------------------------------------
 %% INNER FUNCTION
 %%----------------------------------------------------------------------
+-define(STAT_INTERVAL_10,   10000).
+-define(STAT_INTERVAL_60,   60000).
+-define(STAT_INTERVAL_300, 300000).
+
 after_proc({ok, Pid}) ->
     QueueDir = ?env_queue_dir(leo_storage),
     Managers  = ?env_manager_nodes(leo_storage),
@@ -62,13 +66,13 @@ after_proc({ok, Pid}) ->
     ok = launch_repairer(),
 
     ok = leo_statistics_api:start_link(leo_storage),
-    ok = leo_statistics_metrics_vm:start_link(10000),
-    ok = leo_statistics_metrics_vm:start_link(60000),
-    ok = leo_statistics_metrics_vm:start_link(300000),
-    ok = leo_statistics_metrics_req:start_link(60000),
-    ok = leo_statistics_metrics_req:start_link(300000),
-    ok = leo_storage_mq_statistics:start_link(60000),
-    ok = leo_storage_mq_statistics:start_link(300000),
+    ok = leo_statistics_metrics_vm:start_link(?STAT_INTERVAL_10),
+    ok = leo_statistics_metrics_vm:start_link(?STAT_INTERVAL_60),
+    ok = leo_statistics_metrics_vm:start_link(?STAT_INTERVAL_300),
+    ok = leo_statistics_metrics_req:start_link(?STAT_INTERVAL_60),
+    ok = leo_statistics_metrics_req:start_link(?STAT_INTERVAL_300),
+    ok = leo_storage_mq_statistics:start_link(?STAT_INTERVAL_60),
+    ok = leo_storage_mq_statistics:start_link(?STAT_INTERVAL_300),
 
     ok = leo_storage_mq_client:start(QueueDir),
     ok = leo_redundant_manager_api:start(storage, Managers, QueueDir),
