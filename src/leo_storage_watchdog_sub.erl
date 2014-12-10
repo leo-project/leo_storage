661d2c094d3eb4838e55569db8af619dcbed49ce
Yosuke Hara <yousuke.hara@gmail.com>
2014-12-10 16:54:10 +0900

    Improve the watchdog subscriber

--- src/leo_storage_watchdog_sub.erl
+++ src/leo_storage_watchdog_sub.erl
@@ -70,6 +70,8 @@ start() ->
 handle_notify(?WD_SUB_ID_1,_Alarm,_Unixtime) ->
     %% Increase waiting time of data-compaction/batch-proc
     ok = leo_compact_fsm_controller:incr_waiting_time(),
+    %% Decrease the number of batch-procs
+    ok = leo_compact_fsm_controller:decr_batch_procs(),
     %% Increase waiting time of mq-compsumption
     ok = leo_mq_api:incr_waiting_time(?QUEUE_ID_PER_OBJECT),
     ok = leo_mq_api:incr_waiting_time(?QUEUE_ID_SYNC_BY_VNODE_ID),
@@ -117,6 +119,8 @@ handle_notify(?WD_SUB_ID_2, #watchdog_alarm{state = #watchdog_state{
 handle_notify(?WD_SUB_ID_1,_State,_SafeTimes,_Unixtime) ->
     %% Decrease waiting time of data-compaction/batch-proc
     ok = leo_compact_fsm_controller:decr_waiting_time(),
+    %% Increase the number of batch-procs
+    ok = leo_compact_fsm_controller:incr_batch_procs(),
     %% Decrease waiting time of mq-compsumption
     ok = leo_mq_api:decr_waiting_time(?QUEUE_ID_PER_OBJECT),
     ok = leo_mq_api:decr_waiting_time(?QUEUE_ID_SYNC_BY_VNODE_ID),
