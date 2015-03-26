b0ca55fc8f973675cd7c951ff58556464c4a577d
Yosuke Hara <yousuke.hara@gmail.com>
2015-03-26 12:16:57 +0900

    Fix leo-project/leofs/issues/344

--- src/leo_storage_handler_object.erl
+++ src/leo_storage_handler_object.erl
@@ -577,14 +577,9 @@ head_with_calc_md5(AddrId, Key, MD5Context) ->
 replicate(Object) ->
     %% Transform an object to a metadata
     Metadata = leo_object_storage_transformer:object_to_metadata(Object),
-    Method = case Object#?OBJECT.method of
-                 undefined ->
-                     case Object#?OBJECT.del of
-                         ?DEL_TRUE -> delete;
-                         ?DEL_FALSE -> put
-                     end;
-                 Method_0 ->
-                     Method_0
+    Method = case Object#?OBJECT.del of
+                 ?DEL_TRUE  -> ?CMD_DELETE;
+                 ?DEL_FALSE -> ?CMD_PUT
              end,
     NumOfReplicas = Object#?OBJECT.num_of_replicas,
     AddrId = Metadata#?METADATA.addr_id,
